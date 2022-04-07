bafbb6834e29da5529d36486140d06f970f9f385
usa3amustafa <usa3a3ustafa@outlook.com>
2022-04-08 01:59:53 +0500

    changes

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ reviews.forEach(review => {
   const reply = review.querySelector('.reply')
 
   readReplyBtn.addEventListener('click', () => {
-    console.log(readReplyBtn)
-    console.log(reply)
-
     readReplyBtn.classList.add('hide-reply-btn')
     reply.classList.add('show-reply')
   })
