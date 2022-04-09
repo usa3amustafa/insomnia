5fe15ed91d67b5e83f3300c7055f5cb2bb835c17
usa3amustafa <usa3a3ustafa@outlook.com>
2022-04-09 16:03:57 +0500

    changes

--- app.js
+++ app.js
@@ -9,3 +9,20 @@ reviews.forEach(review => {
     reply.classList.add('show-reply')
   })
 })
+
+const dropdown = document.querySelector('.dropdown')
+const dropdownMenu = document.querySelector('.dropdown-menu')
+const hamburgerMenu = document.querySelector('.hamburger-menu')
+const navLinks = document.querySelector('.nav-links')
+
+document.addEventListener('click', e => {
+  if (e.target.classList.contains('dropdown-btn')) {
+    dropdownMenu.classList.toggle('show-dropdown')
+  } else {
+    dropdownMenu.classList.remove('show-dropdown')
+  }
+})
+
+hamburgerMenu.addEventListener('click', () => {
+  navLinks.classList.toggle('show-nav')
+})
